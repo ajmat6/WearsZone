6b727b6ef5238eca1a61c56a69798c5cd3eb6a43
ajmat6 <114720514+ajmat6@users.noreply.github.com>
2023-07-29 12:12:00 +0530

    file based routing

---
+++ src/app/components/page.jsx
@@ -0,0 +1,16 @@
+// "use client";
+
+import React from 'react'
+
+function about() {
+  console.log("hello")
+  console.log("Here you can write server side code (Backend")
+  return (
+    <div>
+      <p>Hello my name is Ajmat Kathat</p>
+      <button>First Nextjs file based routing</button>
+    </div>
+  )
+}
+
+export default about
