6e0bac519c57aaaaed2266248b333f3e8bf33fba
ajmat6 <114720514+ajmat6@users.noreply.github.com>
2023-08-08 23:04:12 +0530

    Navbar creation + css error resolved

---
+++ src/app/about/page.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Head from 'next/head'
+
+function About() {
+  return (
+    <div>
+      <Head>
+        <title>About Page</title>
+        <meta property='og:title' content='About Page' key="title"/>
+      </Head>
+      <p>Hello my name is Ajmat Kathat</p>
+      <button>First Nextjs file based routing</button>
+    </div>
+  )
+}
+
+export default About
+
+ 
+ 
